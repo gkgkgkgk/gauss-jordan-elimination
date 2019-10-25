1702962efd80437210c2e30a69768681d64cc01a
gkgkgkgk <gkepets@gmail.com>
2019-10-25 01:34:23 -0400

    further reduced byte count

--- js/index.js
+++ js/index.js
@@ -5,29 +5,25 @@ const n4 = [[4, -4, -2, 0], [0, 0, 0, 0], [-2, 2, -6, 0]];
 
 const hello = arr => {
     let a = [...arr];
-    for (let i = 0; i < arr.length; i++) {
-        a = a.slice(0, i).concat(a.slice(i, a.length).sort((a, b) => { return b[i] - a[i] }));
-        if (a[i][i] !== 1) {
+    let l = arr[0].length;
+    let o = a.length;
+    for (let i = 0; i < o; i++) {
+        a = a.slice(0, i).concat(a.slice(i, o).sort((a, b) => b[i] - a[i]));
+        if (a[i][i] != 1) {
             let d = a[i][i];
-            for (let j = 0; j < a[i].length; j++) {
-                a[i][j] = (d == 0 ? 0 : a[i][j] / d);
-            }
+            for (let j = 0; j < l; j++) { a[i][j] = (d == 0 ? 0 : a[i][j] / d); }
         }
-        for (let j = i + 1; j < a.length; j++) {
+        for (let j = i + 1; j < o; j++) {
             let m = a[i][i] == 0 ? 0 : (-a[j][i]) / a[i][i];
-            for (let k = 0; k < arr[j].length; k++) {
-                a[j][k] += m * a[i][k];
-            }
+            for (let k = 0; k < l; k++) { a[j][k] += m * a[i][k]; }
         }
     }
-    for (let i = a.length - 2; i >= 0; i--) {
-        for (let j = a[i].length - 1; j >= 0; j--) {
-            if (j != a[i].length - 1 && a[i][j] == 1) break;
-            if (j != a[i].length - 1) {
+    for (let i = o - 2; i >= 0; i--) {
+        for (let j = l - 1; j >= 0; j--) {
+            if (j != l - 1 && a[i][j] == 1) break;
+            if (j != l - 1) {
                 let m = a[i][i] == 0 ? 0 : (-a[i][j]) / a[i][i];
-                for (let k = 0; k < arr[j].length; k++) {
-                    a[i][k] += m * a[j][k];
-                }
+                for (let k = 0; k < l; k++) { a[i][k] += m * a[j][k]; }
             }
         }
     }
