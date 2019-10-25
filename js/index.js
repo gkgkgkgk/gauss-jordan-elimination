fce59552bf3398e9bf3e777fa04a5a524d0589a2
gkgkgkgk <gkepets@gmail.com>
2019-10-25 01:01:14 -0400

    it works but its messy af

--- js/index.js
+++ js/index.js
@@ -6,7 +6,20 @@ const hello = arr => {
     console.log("original array:");
     console.log(arr);
     let arrAug = [...arr];
+    // iterate throug columns
     for (let i = 0; i < arr.length; i++) {
+        console.log("column " + i);
+        console.log(arrAug);
+        let untouchedArr = arrAug.slice(i, arrAug.length);
+        untouchedArr.sort((a, b) => { return b[i] - a[i] });
+        console.log("untouched array");
+        console.log(untouchedArr);
+        let sliced = arrAug.slice(0, i);
+        console.log("sliced array");
+        console.log(sliced);
+        arrAug = sliced.concat(untouchedArr);
+        console.log("sorted arr: ");
+        console.log(arrAug);
         // create the leading 1
         let d = 1;
         if (arrAug[i][i] !== 1) {
@@ -31,11 +44,31 @@ const hello = arr => {
             console.log("created zero under 1:");
             console.log(arrAug);
         }
-
-
-
-
     }
+
+    console.log(arrAug);
+    for (let i = arrAug.length - 2; i >= 0; i--) {
+        for (let j = arrAug[i].length - 1; j >= 0; j--) {
+            if (arrAug[i][j] == 1) {
+                break;
+            }
+            if (j != arrAug[i].length - 1) {
+                console.log("looking at value: ");
+                console.log(arrAug[i][j]);
+                let pZero = arrAug[i][j];
+                let multiple = (-pZero) / arrAug[i][i];
+                console.log("found multiple " + multiple + " for row " + arrAug[j] + " #" + j);
+                for (let k = 0; k < arr[j].length; k++) {
+                    console.log("adding " + (multiple * arrAug[j][k]) + " to " + arrAug[i][k])
+                    arrAug[i][k] += (multiple * arrAug[j][k]);
+
+                }
+                console.log("created zero under 1:");
+                console.log(arrAug);
+            }
+        }
+    }
+
     console.log(arrAug);
 
 }
